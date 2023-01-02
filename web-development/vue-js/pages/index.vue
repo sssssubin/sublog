0d5fd5bf09a139b34bea475bf200df14e3933843
forest-k <bstnqls@naver.com>
2023-01-02 11:53:34 +0900

    sass 설치

--- web-development/vue-js/pages/index.vue
+++ web-development/vue-js/pages/index.vue
@@ -1,59 +1,48 @@
 <template>
   <section class="container">
     <div>
-      <app-logo/>
-      <h1 class="title">
-        vue-js
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
+      <app-logo />
+      <h1 class="title">sublog</h1>
+      <h2 class="subtitle">Nuxt.js project</h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
+          >Documentation</a
+        >
         <a
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
-          class="button--grey">GitHub</a>
+          class="button--grey"
+          >GitHub</a
+        >
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import AppLogo from "~/components/AppLogo.vue";
 
 export default {
   components: {
-    AppLogo
-  }
-}
+    AppLogo,
+  },
+};
 </script>
 
-<style>
+<style lang="scss">
 .container {
   min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+  max-width: $max-width;
+  margin: 0 auto;
 }
 
 .subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
+  @include font($type: sub-tit);
+  color: color(secondary);
   word-spacing: 5px;
   padding-bottom: 15px;
 }
@@ -62,4 +51,3 @@ export default {
   padding-top: 15px;
 }
 </style>
-
