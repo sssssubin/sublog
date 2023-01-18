fed14d40bdd2b4bc57782a60cd841bd3e6379c1a
forest-k <bstnqls@naver.com>
2023-01-18 16:51:50 +0900

    image 경로 수정

--- web-development/vue-js/pages/study/interaction/index.vue
+++ web-development/vue-js/pages/study/interaction/index.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="articles">
-    <nuxt-link  v-for="(article, i) in articles" :key="i" :to="article.to" class="article">
+    <nuxt-link
+      v-for="(article, i) in articles"
+      :key="i"
+      :to="article.to"
+      class="article"
+    >
       <div class="thumbnail-img">
         <img class="thumbnail-img" :alt="article.alt" :src="article.src" />
       </div>
-      <p>{{article.tit}}</p>
-    </nuxt-link>   
+      <p>{{ article.tit }}</p>
+    </nuxt-link>
   </div>
 </template>
 <script>
@@ -14,19 +19,19 @@ export default {
     return {
       articles: [
         {
-          to: '/study/interaction/moreView',
-          src: '/thumbnail1.png',
-          alt: '',
-          tit: '텍스트 더 보기'
+          to: "/study/interaction/moreView",
+          src: "thumbnail1.png",
+          alt: "",
+          tit: "텍스트 더 보기",
         },
         {
-          to: '/study/interaction/theTermAgree',
-          src: '/thumbnail2.png',
-          alt: '',
-          tit: '전체 동의 & 버튼 활성화'
-        }
-      ]
+          to: "/study/interaction/theTermAgree",
+          src: "thumbnail2.png",
+          alt: "",
+          tit: "전체 동의 & 버튼 활성화",
+        },
+      ],
     }
-  }
+  },
 }
 </script>
