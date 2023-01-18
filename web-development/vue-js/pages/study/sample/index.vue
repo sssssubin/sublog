1c8fac77c965056bae19214e001acfcf6111ac88
forest-k <bstnqls@naver.com>
2023-01-18 16:26:29 +0900

    add sample page

---
+++ web-development/vue-js/pages/study/sample/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="articles">
+    <nuxt-link  v-for="(article, i) in articles" :key="i" :to="article.to" class="article">
+      <div class="thumbnail-img">
+        <img class="thumbnail-img" :alt="article.alt" :src="article.src" />
+      </div>
+      <p>{{article.tit}}</p>
+    </nuxt-link>   
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      articles: [
+        {
+          to: '',
+          src: '',
+          alt: '',
+          tit: 'sample'
+        },
+      ]
+    }
+  }
+}
+</script>
