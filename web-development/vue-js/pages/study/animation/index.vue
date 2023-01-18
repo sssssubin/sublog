bc956776fcc1a1be3d0b1f9a534cd5db9b83032b
forest-k <bstnqls@naver.com>
2023-01-18 16:53:40 +0900

    image 경로 수정

--- web-development/vue-js/pages/study/animation/index.vue
+++ web-development/vue-js/pages/study/animation/index.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="articles">
-    <nuxt-link v-for="(article, i) in articles" :key="i" :to="article.to" class="article">
+    <nuxt-link
+      v-for="(article, i) in articles"
+      :key="i"
+      :to="article.to"
+      class="article"
+    >
       <div class="thumbnail-img">
         <img class="thumbnail-img" :alt="article.alt" :src="article.src" />
-      </div>      
-      <p>{{article.tit}}</p>
-    </nuxt-link>   
+      </div>
+      <p>{{ article.tit }}</p>
+    </nuxt-link>
   </div>
 </template>
 <script>
@@ -14,19 +19,19 @@ export default {
     return {
       articles: [
         {
-          to: '/study/animation/shapeAni',
-          src: '/motion1.png',
-          alt: '',
-          tit: '도형 애니메이션'
+          to: "/study/animation/shapeAni",
+          src: "motion1.png",
+          alt: "",
+          tit: "도형 애니메이션",
         },
         {
-          to: '/study/animation/',
-          src: '',
-          alt: '',
-          tit: ''
+          to: "/study/animation/",
+          src: "",
+          alt: "",
+          tit: "",
         },
-      ]
-    }
-  }
-}
+      ],
+    };
+  },
+};
 </script>
