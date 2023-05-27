553e697c4eb54ece14069151bce5bb2c9e5a2300
forest-k <bstnqls@naver.com>
2023-05-28 00:32:15 +0900

    BaseSlider test

--- web-development/vue-js/components/BaseSlider.vue
+++ web-development/vue-js/components/BaseSlider.vue
@@ -118,7 +118,7 @@ export default {
         .swiper-slide {
             text-align: center;
             font-size: 18px;
-
+            background: #fff;
             /* Center slide text vertically */
             display: -webkit-box;
             display: -ms-flexbox;
